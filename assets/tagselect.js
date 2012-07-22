0464235b431ff108707489ca8ab266e12e2189a5
Jacob Buck <buck.jacob@gmail.com>
2012-07-23 09:50:38 +1200

    v1.0.4 - Conforming to WordPress Coding Standards. Added reademe.txt.

--- assets/tagselect.js
+++ assets/tagselect.js
@@ -1,40 +1,29 @@
-jQuery(function ($) {
-
-	$(".tagselect-wrap").each(function () {
-		
-		var select = $(".tagselect-select:first", this),
-			is_mobile = $("body").hasClass("mobile");
-		
-		if (! is_mobile)
-			select.chosen({allow_single_deselect : true});
-		
-		if ($(".tagselect-add-wrap:first", this).length) {
-			
-			function add_terms () {
-				var add_values = add_text.val().split(",");
-				add_text.val("");
-				if (add_values.length) {
-					for (var key = 0, val; key < add_values.length; key++)
-						if (val = add_values[key].replace(/(^\s*|\s*$)/, ""))
-							select.prepend( $("<option>", {"selected":"selected"}).text(val) );
-					if (! is_mobile)
-						select.trigger("liszt:updated");
+jQuery( function ($) {
+	$('.tagselect-wrap').each( function () {
+		var select = $( '.tagselect-select:eq(0)', this ),
+		    is_mobile = $('body').hasClass('mobile');
+		if ( ! is_mobile )
+			select.chosen( { allow_single_deselect: true } );
+		if ( $('.tagselect-add-wrap:first', this).length ) {
+			function add_terms() {
+				var add_values = add_text.val().split(',');
+				add_text.val('');
+				if ( add_values.length ) {
+					for ( var key = 0, val; key < add_values.length; key++ )
+						if ( val = add_values[ key ].replace( /(^\s*|\s*$)/, '' ) )
+							select.prepend( $( '<option>', { 'selected': 'selected' } ).text( val ) );
+					if ( ! is_mobile )
+						select.trigger('liszt:updated');
 				}
 				return false;
 			}
-			
-			var add_button = $(".tagselect-add-button:first", this),
-				add_text = $(".tagselect-add-text:first", this);
-				
-			add_button.click(add_terms);
-			
-			add_text.keypress(function(event) {
-				if (event.which == 13)
-					return add_terms(event);
-			});
-			
+			var add_button = $( '.tagselect-add-button:first', this ),
+			    add_text = $( '.tagselect-add-text:first', this );
+			add_button.click( add_terms );
+			add_text.keypress( function ( event ) {
+				if ( 13 === event.which )
+					return add_terms( event );
+			} );
 		}
-		
-	});
-
-});
+	} );
+} );
--- tag-select-meta-box.php
+++ tag-select-meta-box.php
@@ -3,7 +3,7 @@
 Plugin Name: Tag Select Meta Box
 Plugin URI: https://github.com/jacobbuck/wp-tag-select-meta-box
 Description: An alternative post tag and non-hierarchal taxonomy meta box.
-Version: 1.0.3
+Version: 1.0.4
 Author: Jacob Buck
 Author URI: http://jacobbuck.co.nz/
 */
@@ -13,101 +13,98 @@ class TagSelectMetaBox {
 	private $taxonomies;
 	
 	function __construct () {
-		add_action("admin_init", array($this, "admin_init"));
-		add_action("add_meta_boxes", array($this, "add_meta_boxes"));
-		add_action("init", array($this, "init"), 9999);
-		add_action("save_post", array($this, "save_post"));
+		add_action( 'admin_init', array( &$this, 'admin_init' ) );
+		add_action( 'add_meta_boxes', array( &$this, 'add_meta_boxes' ) );
+		add_action( 'init', array( &$this, 'init' ), 9999 );
+		add_action( 'save_post', array( &$this, 'save_post' ) );
 	}
 	
 	function admin_init () {
-		wp_register_script("chosen", plugins_url("assets/chosen/chosen.jquery.min.js", __FILE__), array("jquery"), "0.9.8");
-		wp_register_script("tagselect", plugins_url("assets/tagselect.min.js", __FILE__), array("jquery"), "1.0.2");
-		wp_register_style("chosen", plugins_url("assets/chosen/chosen.min.css", __FILE__), null, "0.9.8");
-		wp_register_style("tagselect", plugins_url("assets/tagselect.min.css", __FILE__), null, "1.0.2");
+		wp_register_script( 'chosen', plugins_url( 'assets/chosen/chosen.jquery.min.js', __FILE__ ), array('jquery'), '0.9.8' );
+		wp_register_style( 'chosen', plugins_url( 'assets/chosen/chosen.min.css', __FILE__ ), array(), '0.9.8' );
+		wp_register_script( 'tagselect', plugins_url( 'assets/tagselect.min.js', __FILE__ ), array('jquery'), '1.0.4' );
+		wp_register_style( 'tagselect', plugins_url( 'assets/tagselect.min.css', __FILE__ ), array(), '1.0.4' );
 	}
 	
-	function add_meta_boxes ($post_type) {
-		foreach ($this->taxonomies as $tax) {
-			if (is_object_in_taxonomy($post_type, $tax->name)) {
-				add_action("admin_enqueue_scripts", array($this, "admin_enqueue_scripts"));
+	function add_meta_boxes ( $post_type ) {
+		foreach ( (array) $this->taxonomies as $tax ) {
+			$tax_name = $tax->name;
+			if ( is_object_in_taxonomy( $post_type, $tax_name ) ) {
+				add_action( 'admin_enqueue_scripts', array( &$this, 'admin_enqueue_scripts' ) );
 				add_meta_box( 
-					"tagselect-".$tax->name,
-					__(empty($tax->tagselect["singular"]) ? $tax->label : $tax->labels->singular_name),
-					array($this, "meta_box_callback"),
+					"tagselect-$tax_name",
+					__( empty( $tax->tagselect['singular'] ) ? $tax->label : $tax->labels->singular_name ),
+					array( &$this, 'meta_box_callback' ),
 					null,
-					"side",
-					"default",
-					array("taxonomy" => $tax)
+					'side',
+					'default',
+					array( 'taxonomy' => $tax )
 				);
-				remove_meta_box("tagsdiv-".$tax->name, $post_type, "side");
+				remove_meta_box( "tagsdiv-$tax_name", $post_type, 'side' );
 			}
 		}
 	}
 	
 	function init () {
-		$this->taxonomies = get_taxonomies(array("hierarchical" => false, "show_ui" => true), "object");
+		$this->taxonomies = get_taxonomies(
+			array(
+				'hierarchical' => false,
+				'show_ui' => true
+			), 
+			'object'
+		);
 	}
 	
-	function save_post ($post_id) {
-		if (defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+	function save_post ( $post_id ) {
+		if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE )
 			return;
-		foreach ($this->taxonomies as $tax) {
+		foreach ( $this->taxonomies as $tax ) {
 			$tax_name = $tax->name;
-			if (isset($_POST["tagselect-$tax_name-select"])) {
-				wp_set_object_terms($post_id, $_POST["tagselect-$tax_name-select"], $tax_name, false);
-			}
+			if ( isset( $_POST["tagselect-$tax_name-select"] ) )
+				wp_set_object_terms( $post_id, $_POST["tagselect-$tax_name-select"], $tax_name, false );
 		}
 	}
 		
 	function admin_enqueue_scripts () {
-		if (! wp_is_mobile()) {
-			wp_enqueue_script("chosen");
-			wp_enqueue_style("chosen");
+		if ( ! wp_is_mobile() ) {
+			wp_enqueue_script('chosen');
+			wp_enqueue_style('chosen');
 		}
-		wp_enqueue_script("tagselect");
-		wp_enqueue_style("tagselect");
+		wp_enqueue_script('tagselect');
+		wp_enqueue_style('tagselect');
 	}
 	
-	function meta_box_callback ($post, $box) {
-		$tax = $box["args"]["taxonomy"];	
-		extract(wp_parse_args(
-			empty($tax->tagselect) ? array() : $tax->tagselect,
+	function meta_box_callback ( $post, $box ) {
+		$tax = $box['args']['taxonomy'];	
+		extract( wp_parse_args(
+			empty( $tax->tagselect ) ? array() : $tax->tagselect,
 			array(
-				"singular" => false,
-				"hide_add" => false
+				'singular' => false,
+				'hide_add' => false
 			)
-		));
-		$disabled = ! current_user_can($tax->cap->assign_terms);
+		) );
+		$disabled = ! current_user_can( $tax->cap->assign_terms );
 		$terms = get_terms(
 			$tax->name,
-			array("hide_empty" => false)
+			array( 'hide_empty' => false )
 		);
 		$post_terms = wp_get_object_terms(
 			$post->ID,
 			$tax->name,
-			array("fields" => "slugs")
+			array( 'fields' => 'slugs' )
 		);
-		?>
-		<div class="tagselect-wrap">
-			<div class="tagselect-select-wrap">
-				<?php 
-				echo "<select class=\"tagselect-select\" name=\"".$box["id"]."-select[]\"".($disabled ? " disabled" : "").($singular ? "" : " multiple")." data-placeholder=\"".__("Choose a ".$tax->labels->singular_name)."&hellip;\">\n";
-				if ($singular) {
-					echo "<option></option>\n";
-				}
-				foreach ($terms as $term) {
-					echo "<option value=\"$term->slug\"".(in_array($term->slug, $post_terms) ? " selected=\"selected\"" : "").">".__($term->name)."</option>\n";
-				}
-				echo "</select>\n";
-				?>
-			</div>
-			<?php if (! ($hide_add || $disabled)) { ?>
-				<div class="tagselect-add-wrap hide-if-no-js">
-					<p><input type="text" class="tagselect-add-text" name="<?php echo $box["id"]; ?>-add-text" value=""> <button class="button tagselect-add-button" name="<?php echo $box["id"]; ?>-add-button"><?php _e("Add"); ?></button></p>
-				</div>
-			<?php } ?>
-		</div>
-		<?php
+		echo '<div class="tagselect-wrap"><div class="tagselect-select-wrap">';
+		echo '<select class="tagselect-select" name="' . $box['id'] . '-select[]"' . ( $disabled ? ' disabled' : '' ) . ( $singular ? '' : ' multiple' ) . ' data-placeholder="' . __("Choose a {$tax->labels->singular_name}") . '&hellip;">';
+		if ( $singular ) 
+			echo '<option value=""></option>';
+		foreach ( (array) $terms as $term ) 
+			echo "<option value=\"{$term->slug}\"" . ( in_array( $term->slug, $post_terms ) ? ' selected="selected"' : '' ) . '>' . __( $term->name ) . '</option>';
+		echo '</select></div>';
+		if ( ! ( $hide_add || $disabled ) ) {
+			echo '<div class="tagselect-add-wrap hide-if-no-js"><p><input type="text" class="tagselect-add-text" name="' . $box['id'] . '-add-text" value="">';
+			echo '<button class="button tagselect-add-button" name="' . $box['id'] . '-add-button">' . __('Add') . '</button></p></div>';
+		}
+		echo '</div>';
 	}
 	
 }
